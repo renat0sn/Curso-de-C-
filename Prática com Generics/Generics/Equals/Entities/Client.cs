d4a04cd6af132d899dadb262eda1c2b13b7faec2
Renato Nakano <renato.sousa.gc@hotmail.com>
2020-05-26 16:45:33 -0300

    Prática com Equals e GetHashCode

---
+++ Prática com Generics/Generics/Equals/Entities/Client.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EqualsEGetHashCode
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Client(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Client))
+            {
+                throw new ArgumentException("Argumento passado é incompatível");
+            }
+
+            Client c = obj as Client;
+
+            return Email.Equals(c.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ Prática com Generics/Generics/Equals/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using EqualsEGetHashCode;
+
+namespace Equals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, email;
+            Console.WriteLine("Cliente 1:");
+            Console.Write("Nome: ");
+            name = Console.ReadLine();
+            Console.Write("Email: ");
+            email = Console.ReadLine();
+            Client c1 = new Client(name, email);
+
+            Console.WriteLine("\nCliente 2:");
+            Console.Write("Nome: ");
+            name = Console.ReadLine();
+            Console.Write("Email: ");
+            email = Console.ReadLine();
+            Client c2 = new Client(name, email);
+
+            Console.WriteLine("\nTESTE DE IGUALDADE:");
+            Console.WriteLine("Equals: " + c1.Equals(c2));
+
+            Console.WriteLine("GetHashCode: ");
+            Console.WriteLine("c1: " + c1.GetHashCode());
+            Console.WriteLine("c2: " + c2.GetHashCode());
+        }
+    }
+}
